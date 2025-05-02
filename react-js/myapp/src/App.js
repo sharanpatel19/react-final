6a506d0aa376602b371d73e2567533ae2734ac90
sharanpatel19 <sharanpatel1946@gmail.com>
2025-05-02 10:29:05 +0530

    done

---
+++ react-js/myapp/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Navbar from './Components/Navbar';
+import Textform from './Components/Textform';
+function App() {
+  return ( 
+    <>
+  <Navbar title="textutiles"/>
+ 
+  <div className='container'>
+  <Textform heading="Enter the text to analyze below"/>
+
+  </div>
+  
+
+</>
+  );
+}
+export default App;
